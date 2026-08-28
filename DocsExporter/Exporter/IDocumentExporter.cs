e55d63e0d8aa241e9dc4653eea5da4bf6317271a
Murali47k <murali47karthick@gmail.com>
2026-08-28 21:01:24 +0530

    made Interface for DocsExporter

--- DocsExporter/Exporter/IDocumentExporter.cs
+++ DocsExporter/Exporter/IDocumentExporter.cs
@@ -5,7 +5,8 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace Exporter;
-internal interface IDocumentExporter
+public interface IDocumentExporter
 {
+    string Export(Document document);
 
 }
