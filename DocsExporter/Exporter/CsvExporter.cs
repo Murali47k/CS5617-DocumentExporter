ad2cbec70e6efcca803505960f3c2ab3752c9413
Murali47k <murali47karthick@gmail.com>
2026-08-28 21:00:10 +0530

    made CSV Exporter

--- DocsExporter/Exporter/CsvExporter.cs
+++ DocsExporter/Exporter/CsvExporter.cs
@@ -5,6 +5,30 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace Exporter;
-internal class CsvExporter
+public sealed class CsvExporter : IDocumentExporter
 {
+    public string Export(Document document)
+    {
+        var sb = new StringBuilder();
+
+        sb.AppendLine(CsvFormat(document.Title));
+
+        foreach (string row in document.Rows)
+        {
+            sb.AppendLine(CsvFormat(row));
+        }
+        return sb.ToString();
+    }
+
+    public static string CsvFormat(string input)
+    {
+        if (input.Contains(",") || input.Contains("\"") || input.Contains("\n"))
+        {
+            // Escape double quotes by doubling them
+            string escapedInput = input.Replace("\"", "\"\"");
+            return $"\"{escapedInput}\"";
+        }
+        return input;
+    }
 }
+
