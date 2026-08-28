66cd2cb12aad0de2609da115a499156c9b46d17b
Murali47k <murali47karthick@gmail.com>
2026-08-28 20:59:27 +0530

    made Text Exporter

--- DocsExporter/Exporter/TextExporter.cs
+++ DocsExporter/Exporter/TextExporter.cs
@@ -5,6 +5,18 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace Exporter;
-internal class TextExporter
+public sealed class TextExporter : IDocumentExporter
 {
+    public string Export(Document document)
+    {
+        var sb = new StringBuilder();
+
+        sb.AppendLine(document.Title);
+
+        foreach (string row in document.Rows)
+        {
+            sb.AppendLine(row);
+        }
+        return sb.ToString();
+    }
 }
