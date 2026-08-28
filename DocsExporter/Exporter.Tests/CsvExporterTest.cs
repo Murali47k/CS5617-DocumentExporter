c36b0fa78d1d56096fe808a196f4c2c579900780
Murali47k <murali47karthick@gmail.com>
2026-08-28 21:43:42 +0530

    made test cases for CsvExporter

--- DocsExporter/Exporter.Tests/CsvExporterTest.cs
+++ DocsExporter/Exporter.Tests/CsvExporterTest.cs
@@ -1,10 +1,78 @@
-﻿namespace Exporter.Tests;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Exporter;
 
-//[TestClass]
-public class CsvExporterTest
+namespace Exporter.Tests;
+
+[TestClass]
+public class CsvExporterTest : ExporterContractTest
 {
-    //[TestMethod]
-    public void TestMethod1()
+    protected override IDocumentExporter CreateExporter()
     {
+        return new CsvExporter();
     }
+
+    [TestMethod]
+    public void CsvExporterContract()
+    {
+        ExporterContract();
+    }
+
+    [TestMethod]
+    public void CsvExporterFull()
+    {
+        // Arrange
+        var document = new Document(
+            "Formula 1",
+            new List<string>
+            {
+                "RedBull Racing","Mercedes","Ferrari","McLaren"
+            }
+        );
+
+        IDocumentExporter exporter = CreateExporter();
+
+        // Act
+        string result = exporter.Export(document);
+
+        // Assert 
+        Assert.IsTrue(result.Contains("Formula 1"));
+        Assert.IsTrue(result.Contains("RedBull Racing"));
+        Assert.IsTrue(result.Contains("Mercedes"));
+        Assert.IsTrue(result.Contains("Ferrari"));
+        Assert.IsTrue(result.Contains("McLaren"));
+
+    }
+
+    [TestMethod]
+    public void CsvExporterNoContent()
+    {
+        // Arrange
+        var document = new Document(
+            "EmptyList",
+            new List<string> { }
+        );
+
+        IDocumentExporter exporter = CreateExporter();
+
+        // Act
+        string result = exporter.Export(document);
+
+        // Assert 
+        Assert.IsTrue(result.Contains("EmptyList"));
+
+    }
+
+    [TestMethod]
+    public void CsvExporterNoDocument()
+    {
+        // Arrange
+        IDocumentExporter exporter = CreateExporter();
+
+        // Act & Assert
+        Assert.ThrowsException<ArgumentNullException>(
+            () => exporter.Export(null!)
+        );
+
+    }
+
 }
