21f21ddc12002dabe8367ea5833346566685cfe2
Murali47k <murali47karthick@gmail.com>
2026-08-29 10:20:54 +0530

    added comments to Document.cs

--- DocsExporter/Exporter/Document.cs
+++ DocsExporter/Exporter/Document.cs
@@ -1,9 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace Exporter;
 
-namespace Exporter;
-
+/// <summary>
+/// Document represents a simple document with a title and a collection of rows.
+/// </summary>
+/// <param name="Title">Name of the document</param>
+/// <param name="Rows">List of information in document</param>
 public sealed record Document(string Title, IReadOnlyList<string> Rows);
