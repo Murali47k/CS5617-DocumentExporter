38b13c3feb7cf46742a007ae0d1bbf997f053c0a
Murali47k <murali47karthick@gmail.com>
2026-08-29 10:40:37 +0530

    added comments to JsonExporter.cs

--- DocsExporter/Exporter/JsonExporter.cs
+++ DocsExporter/Exporter/JsonExporter.cs
@@ -1,13 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Text.Json;
+﻿using System.Text.Json;
 
 namespace Exporter;
+
+/// <summary>
+/// JsonExporter is an implementation of the IDocumentExporter interface that exports a document to a json format
+/// </summary>
 public sealed class JsonExporter : IDocumentExporter
 {
+    /// <summary> 
+    /// Exports the given Document to a JSON format. 
+    /// The document's title and collection of rows are serialized into a JSON string. 
+    /// </summary>
+    /// <param name="document">A simple document with a title and a collection of rows</param>
     public string Export(Document document)
     {
         ArgumentNullException.ThrowIfNull(document);
