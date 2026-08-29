9a1a4ea68d7e69a06d94e6094e7f10393816a259
Murali47k <murali47karthick@gmail.com>
2026-08-29 10:27:38 +0530

    added comments to TextExporter.cs

--- DocsExporter/Exporter/TextExporter.cs
+++ DocsExporter/Exporter/TextExporter.cs
@@ -1,12 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Text;
 
 namespace Exporter;
+
+/// <summary>
+/// TextExporter is a implementation of the IDocumentExporter interface that exports a Document to a plain text format.
+/// </summary>
 public sealed class TextExporter : IDocumentExporter
 {
+    /// <summary>
+    /// Exports the given Document to a plain text format. 
+    /// The title of the document is placed on the first line, followed by each row of the document on subsequent lines.
+    /// </summary>
+    /// <param name="document">A simple document with a title and a collection of rows</param>
     public string Export(Document document)
     {
         ArgumentNullException.ThrowIfNull(document);
