815f76d092529efcba9a3348479323fcfb552b51
Murali47k <murali47karthick@gmail.com>
2026-08-29 10:22:53 +0530

    added comments to IDocumentExporter.cs

--- DocsExporter/Exporter/IDocumentExporter.cs
+++ DocsExporter/Exporter/IDocumentExporter.cs
@@ -1,10 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace Exporter;
 
-namespace Exporter;
+/// <summary>
+/// Interface for various document exporter implementations. It defines a method to export a document to a specific format.
+/// </summary>
 public interface IDocumentExporter
 {
     string Export(Document document);
